477509b4e102d89a1f70b6926a2fc92d76f66c67
Ilya Mikhailov <mihailoov@gmail.com>
2020-10-22 09:54:29 +0200

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Ilya on 22.10.20.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
